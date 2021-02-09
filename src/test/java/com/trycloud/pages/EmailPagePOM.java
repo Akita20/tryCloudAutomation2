733dfd825ab5fbfea269038dea9c13ebff797c2c
Akita20 <jamila.osmn@gmail.com>
2021-02-09 11:14:15 -0800

    EmailPagePOM

--- src/test/java/com/trycloud/pages/EmailPagePOM.java
+++ src/test/java/com/trycloud/pages/EmailPagePOM.java
@@ -3,24 +3,36 @@ package com.trycloud.pages;
 import com.trycloud.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class EmailPagePOM {
+public class EmailPagePOM extends PageBase {
+    public EmailPagePOM() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
-    WebElement emailIcon= Driver.getDriver().findElement(By.xpath("//div[@class='icon-mail']"));
-    WebElement contactYourMailAcccount = Driver.getDriver().findElement(By.xpath(" //div[@id='emptycontent']/h2"));
-    WebElement autoTab= Driver.getDriver().findElement(By.xpath(" //li[@class='tabs-component-tab is-active']"));
-    WebElement manualTab= Driver.getDriver().findElement(By.xpath("//li[@class='tabs-component-tab']"));
-
-    WebElement nameTab= Driver.getDriver().findElement(By.xpath("/html/body/div[3]/div/form/div/div/section[1]/label[1]"));
-    WebElement userInput= Driver.getDriver().findElement(By.id("//input[@id='auto-name']"));
-    WebElement mailAddress = Driver.getDriver().findElement(By.id("//input[@id='auto-name']"));
-    WebElement mailAddressTab= Driver.getDriver().findElement(By.id("//input[@id='auto-address']"));
-    WebElement password= Driver.getDriver().findElement(By.id("//input[@id='auto-password']"));
-    WebElement passwordTab= Driver.getDriver().findElement(By.id("//input[@id='auto-password']"));
-    WebElement connectTab= Driver.getDriver().findElement(By.xpath("//input[@class='primary']"));
-
-
-
+    @FindBy(xpath = "//div[@class='icon-mail']")
+    public WebElement emailIcon;
+    @FindBy(xpath = " //div[@id='emptycontent']/h2")
+    public WebElement connectYourMailAccount;
+    @FindBy(xpath = "//li[@class='tabs-component-tab is-active']")
+    public WebElement autoTab;
+    @FindBy(xpath = "//li[@class='tabs-component-tab']")
+    public WebElement manualTab;
+    @FindBy(xpath = "//section[@id='auto']/label[1]")
+    public WebElement name;
+    @FindBy(xpath = "//input[@id='auto-name']")
+    public WebElement inputUser;
+    @FindBy(xpath = "//section[@id='auto']/label[2]")
+    public WebElement mailAddress;
+    @FindBy(xpath = "//input[@id='auto-address']")
+    public WebElement inputMailAddress;
+    @FindBy(xpath = "//label[.='Password']")
+    public WebElement password;
+    @FindBy(xpath = "//input[@id='auto-password']")
+    public WebElement inputPassword;
+    @FindBy(xpath = "//input[@class='primary']")
+    public WebElement connectTab;
 
 
 }
