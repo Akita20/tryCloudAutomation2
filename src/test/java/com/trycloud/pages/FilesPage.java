36d7e34ad9e14d9822c9f4cb1b35fafd1403d0b8
Jair Novello <jaja505@hotmail.com>
2021-02-08 22:01:41 -0500

    FilesPage

--- src/test/java/com/trycloud/pages/FilesPage.java
+++ src/test/java/com/trycloud/pages/FilesPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class FilesPage {
+public class FilesPage extends PageBase {
 
     public FilesPage(){
         PageFactory.initElements(Driver.getDriver(), this);
