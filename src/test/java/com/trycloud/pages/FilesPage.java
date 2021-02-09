ed703342152ce6e70933d23d9d474c7dcd6098b9
Karlygash88 <karlygash.ospan@gmail.com>
2021-02-08 21:44:56 -0800

    US3_TC8_1_karlygash.java has been modified, and more WebElements added to FilesPage.java

--- src/test/java/com/trycloud/pages/FilesPage.java
+++ src/test/java/com/trycloud/pages/FilesPage.java
@@ -1,15 +1,23 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilesPage {
 
     public FilesPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "(//a[@aria-label ='Files'])[1]")
+    public WebElement filesButton;
 
     @FindBy(id = "app-navigation-toggle")
     public WebElement navigationIcon;
@@ -46,5 +54,47 @@ public class FilesPage {
 
     @FindBy(xpath = "//button[@class='settings-button']")
     public WebElement settingsButton;
+
+    @FindBy(xpath = "//a[@class='action action-menu permanent']")
+    public List<WebElement> actionIcons;
+
+    @FindBy(xpath = "//a[@class='menuitem action action-delete permanent']")
+    public WebElement deleteFolderButton;
+
+    @FindBy(xpath = "//td[@class='filename ui-draggable ui-draggable-handle']")
+    public List<WebElement> filesList;
+
+    @FindBy(xpath = "//span[@class='nametext extra-data']")
+    public List<WebElement> deletedFiles;
+
+    public void clickFilesButton(){
+        filesButton.click();
+    }
+
+    public void clickAllFilesButton(){
+        allFilesButton.click();
+    }
+
+    public void clickDeleteFolder(){
+        deleteFolderButton.click();
+    }
+
+    public void clickDeletedFilesButton(){
+        deletedFilesButton.click();
+    }
+
+    public WebElement fileToDelete(){
+
+        return filesList.get(0);
+    }
+
+    public void deleteFile(){
+        actionIcons.get(0).click();
+        BrowserUtils.sleep(3);
+        deleteFolderButton.click();
+    }
+
+
+
 
 }
---
+++ src/test/java/com/trycloud/tests/US3_TC8_2_karlygash.java
@@ -0,0 +1,17 @@
+package com.trycloud.tests;
+
+import com.trycloud.pages.FilesPage;
+import com.trycloud.tests.base.TestBase;
+import org.testng.annotations.Test;
+
+public class US3_TC8_2_karlygash extends TestBase {
+
+    @Test
+    public void add_comments_to_file(){
+        login();
+        FilesPage filesPage = new FilesPage();
+
+
+    }
+
+}
