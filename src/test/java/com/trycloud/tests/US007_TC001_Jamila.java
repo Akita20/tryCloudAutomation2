02462d82e040f2e5aa1914b2daba0329ebbb74a8
Akita20 <jamila.osmn@gmail.com>
2021-02-09 16:31:18 -0800

    US007_001

--- src/test/java/com/trycloud/tests/US007_TC001_Jamila.java
+++ src/test/java/com/trycloud/tests/US007_TC001_Jamila.java
@@ -1,7 +1,15 @@
 package com.trycloud.tests;
 
+import com.trycloud.pages.PageBase;
 import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.BrowserUtils;
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Objects;
 
 public class US007_TC001_Jamila extends TestBase {
     @Test
@@ -10,10 +18,24 @@ public class US007_TC001_Jamila extends TestBase {
 
         //1. Login as a user
         login();
+        BrowserUtils.sleep(2);
         //2. Click magnifier icon on the right top
+        PageBase pageBase= new PageBase();
 
+        pageBase.buttonSearchClick();
         //3. Search any existing file/folder/user name
+        // search file name "watan"
+        WebElement searchBox= Driver.getDriver().findElement(By.xpath("//input[@type='search']"));
+        searchBox.sendKeys("watan");
+        searchBox.click();
+        String actualTitle=Driver.getDriver().getTitle();
+        BrowserUtils.sleep(2);
+        WebElement expectedTitle= Driver.getDriver().findElement(By.xpath("ul[@class='unified-search__results unified-search__results-files']"));
+
+
+       //ul[@class='unified-search__results unified-search__results-files']
        //4. Verify the app displays the expected result option
+        Assert.assertEquals(actualTitle,expectedTitle);
 
 
     }
