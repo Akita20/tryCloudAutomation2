f11e222ba3f5c49feb2ba01d67cd27e951b996e6
esma17 <milkysf@gmail.com>
2021-02-12 11:01:29 -0600

    added some comments to the US005_TC001_esma

--- src/test/java/com/trycloud/tests/US005_TC001_esma.java
+++ src/test/java/com/trycloud/tests/US005_TC001_esma.java
@@ -2,6 +2,7 @@ package com.trycloud.tests;
 
 import com.trycloud.pages.PageBase;
 import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.Driver;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -16,11 +17,14 @@ public class US005_TC001_esma extends TestBase {
         login();
         PageBase page=new PageBase();
         page.buttonContactMenuClick();
-        //2. Click “Contacts” module
-        page.contactsUnderContactMenuBar.get(0).click();
+        //2. Click “Contacts” module and click to Dory Semal contact
         String name= page.selectContactFromContactMenuBar("Dory");
         //3. Verify the page tile is Contents module’s tile
         Assert.assertTrue(driver.getTitle().contains(name));
+
+        //the test fails I tried manually and we are unable to click to contacts under Contact Menu bar
+        //in the other environment we had a button on the contacts that was enabling us to go to talk page
+        //of that certain contact
 
     }
 
