55bb5586233bf00931297ff1d34f46503bdda1c4
Akita20 <jamila.osmn@gmail.com>
2021-02-11 22:29:11 -0800

    US007_001

--- src/test/java/com/trycloud/tests/US007_TC001_Jamila.java
+++ src/test/java/com/trycloud/tests/US007_TC001_Jamila.java
@@ -3,22 +3,28 @@ package com.trycloud.tests;
 import com.trycloud.pages.PageBase;
 import com.trycloud.tests.base.TestBase;
 import com.trycloud.utilities.BrowserUtils;
+import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class US007_TC001_Jamila extends TestBase {
+
+
     @Test
     public void TC001() {
         //        Test case #1 - Verify users can search any files/folder/users from the search box.
 
         //1. Login as a user
         login();
-        BrowserUtils.sleep(2);
+        BrowserUtils.sleep(1);
         //2. Click magnifier icon on the right top
         PageBase pageBase= new PageBase();
 
@@ -28,15 +34,38 @@ public class US007_TC001_Jamila extends TestBase {
         WebElement searchBox= Driver.getDriver().findElement(By.xpath("//input[@type='search']"));
         searchBox.sendKeys("watan");
         searchBox.click();
+
         String actualTitle= Driver.getDriver().findElement(By.xpath("//strong[.='watan']")).getText();
         BrowserUtils.sleep(2);
         String expectedTitle= "watan";
-
-
-       //ul[@class='unified-search__results unified-search__results-files']
-       //4. Verify the app displays the expected result option
+        //4. Verify the app displays the expected result option
         Assert.assertEquals(actualTitle,expectedTitle);
 
 
+
+
+    }
+    @Test
+    public void tc2() {
+        //        Test case #1 - Verify users can search any files/folder/users from the search box.
+
+        //1. Login as a user
+        login();
+        BrowserUtils.sleep(1);
+        //2. Click magnifier icon on the right top
+        PageBase pageBase= new PageBase();
+
+        pageBase.buttonSearchClick();
+        //3. Search any existing file/folder/user name
+
+       // search user name
+        WebElement searchBox= Driver.getDriver().findElement(By.xpath("//input[@type='search']"));
+      //  Driver.getDriver()
+         searchBox.sendKeys(ConfigurationReader.getProperty("username"));
+         searchBox.click();
+         String actualTitle1=Driver.getDriver().findElement(By.xpath("//input[@id='user']")).getText();
+         String expectedTitle1="username";
+
+          Assert.assertEquals(actualTitle1,expectedTitle1);
     }
 }
